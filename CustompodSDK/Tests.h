dcc79f3ab0e510157e80bb56d9db44c8d63f7e23
Santhosh Gandekattera <sgandekattera@innominds.com>
2018-11-14 15:10:56 +0530

    initial commit

---
+++ CustompodSDK/Tests.h
@@ -0,0 +1,13 @@
+//
+//  Tests.h
+//  Custompod-Mac
+//
+//  Created by Santhosh Gandekattera on 14/11/18.
+//  Copyright © 2018 Santhosh Gandekattera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tests : NSObject
++ (int)getMyValue: (int)myId;
+@end
